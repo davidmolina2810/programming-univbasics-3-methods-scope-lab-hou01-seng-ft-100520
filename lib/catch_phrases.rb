bec3fdc229727cb9d29b44dbcf3d8d80977cac03
David Molina <davidmolina2810@gmail.com>
2020-08-08 19:36:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,15 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+#method that will output "It's Dangerous To Go Alone! Take This."
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+#method that will call #mario, #toadstool, #link and output their strings
+def all_phrases
+  mario
+  toadstool
+  link
+end
